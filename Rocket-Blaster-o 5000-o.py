6c641914244066ea301f2751b9bf3f4dc94a2220
Cameron Clark <cameron.clark@kearsarge.org>
2013-12-06 08:52:11 -0500

    player death

--- Rocket-Blaster-o 5000-o.py
+++ Rocket-Blaster-o 5000-o.py
@@ -2,8 +2,8 @@ import pygame, sys, math, random, time
 from pygame.locals import *
 from Player import Player
 from Shot import Shot
-from Alien import Alien
 from Score import Score
+from Alien import Alien
 if not pygame.font: print 'Warning, fonts disabled'
 if not pygame.mixer: print 'Warning, sound disabled'
 
@@ -13,6 +13,8 @@ size = width, height
 
 pygame.init()
 
+clock = pygame.time.Clock()
+
 screen = pygame.display.set_mode(size)
 bgColor = r,g,b = 0,0,0
 bgImage = pygame.image.load("Resources/Start_Screen/StartScreen.png")
@@ -21,7 +23,6 @@ bgRect = bgImage.get_rect()
 player = Player("Resources/Player/Player.png", (5,5), (100, height/2), (100,40))
 shot = Shot("Resources/Shot/Rocket.png")
 score = Score()
-
 start = False
 while True:
     while not start:
@@ -31,13 +32,13 @@ while True:
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_RETURN:
                         start = True
-            
+                        bgImage = pygame.image.load("Resources/Background/Background.png")
+
             screen.blit(bgImage, bgRect)
             pygame.display.flip()
             clock.tick(60)
 
-            bgImage = pygame.image.load("Resources/Background/Background.png")
-
+    st = time.time()
     while start:
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
@@ -46,9 +47,6 @@ while True:
                     
                 if event.key == pygame.K_s or event.key == pygame.K_DOWN:
                     player.direction("down")
-                    
-                if event.key == pygame.K_RETURN or event.key == pygame.K_SPACE:
-                    bgImage = pygame.image.load("Background.png")
             
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_w or event.key == pygame.K_UP:
@@ -56,20 +54,25 @@ while True:
                     
                 if event.key == pygame.K_s or event.key == pygame.K_DOWN:
                     player.direction("stop down")
+    
+        if player.living = False:
+            bgImage = pygame.image.load("Resources/End_Screen/EndScreen.png")
+            
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE or event.key == pygame.K_RETURN
+                    bgImage = pygame.image.load("Resources/Start_Screen/StartScreen.png")
                     
+                if event.key == pygame.K_Q or event.ket == pygame.K_ESCAPE:
+                    quit()
         
         player.update()
         shot.update()
         alien.update()
         score.update()
-        endScreen.update()
-        startScreen.update()
         
         pygame.time.get_ticks()
-        st = time.time()
         timeSinceStart = time.time()-st
         score.increase()
-        
         
         screen.fill(bgColor)
         screen.blit(score.image, score.rect)
