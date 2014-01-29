914ba98bc9fc88a46a995c7bf7a146388978cd8e
Cameron Clark <cameron.clark@kearsarge.org>
2014-01-29 08:56:11 -0500

    Smooth background moving

--- Background.py
+++ Background.py
@@ -16,7 +16,7 @@ class Background():
     def update(self):
         self.move()
         print self.rect.x
-        if self.rect.x <= -950:
+        if self.rect.x <= -1550:
             print "blam"
             self.reset()
         
